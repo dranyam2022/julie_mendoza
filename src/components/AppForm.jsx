eb281c85341d0fa7e6ff7efdc778b0dd38b2535e
dranyam2022 <maynardescalante@gmail.com>
2023-10-11 16:13:32 +0800

    corrected typos

--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -32,7 +32,7 @@ function AppForm() {
           <Form.Select aria-label="Default select example">
             <option value="...">...</option>
             <option value="visual">visual</option>
-            <option value="auditory">Female</option>
+            <option value="auditory">auditory</option>
             <option value="reading/writing">reading/writing</option>
             <option value="kinesthetic">kinesthetic</option>
           </Form.Select>
